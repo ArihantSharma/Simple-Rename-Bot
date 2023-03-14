947b573f440d609ae84e8d464b6cba1dca84efa5
Arihant Sharma <71224541+ArihantSharma@users.noreply.github.com>
2023-03-14 13:14:42 +0530

    Update start_text.py

--- main/start_text.py
+++ main/start_text.py
@@ -3,7 +3,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from config import ADMIN
 
 
-@Client.on_callback_query(filters.regex("start"))
+@Client.on_callback_query(filters.command("start"))
 async def start(bot, msg, cb=True):   
     txt=f"hai {msg.from_user.mention} i am simple rename bot with personal usage.\nthis bot is made by <b><a href=https://t.me.com/voatxm>VOAT</a></b>"                                     
     button= [[
@@ -19,7 +19,7 @@ async def start(bot, msg, cb=True):
         await msg.reply_text(text=txt, reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview = True, parse_mode=enums.ParseMode.HTML)
 
 
-@Client.on_callback_query(filters.regex("help"))
+@Client.on_callback_query(filters.command("help"))
 async def help(bot, msg):
     txt = "just send a file and /rename <new name> with replayed your file\n\n"
     txt += "send photo to set thumbnail automatic \n"
@@ -32,7 +32,7 @@ async def help(bot, msg):
     await msg.message.edit(text=txt, reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview = True)
 
 
-@Client.on_callback_query(filters.regex("about"))
+@Client.on_callback_query(filters.command("about"))
 async def about(bot, msg):
     me=await bot.get_me()
     Master=f"<a href=https://t.me/voatxm>VOAT</a>"  
@@ -45,7 +45,7 @@ async def about(bot, msg):
     await msg.message.edit(text=txt, reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview = True, parse_mode=enums.ParseMode.HTML)
 
 
-@Client.on_callback_query(filters.regex("del"))
+@Client.on_callback_query(filters.command("del"))
 async def closed(bot, msg):
     try:
         await msg.message.delete()
