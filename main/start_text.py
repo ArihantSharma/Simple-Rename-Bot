9850ca623fe3dbd7b297b4913847aa4dcfa048fb
Arihant Sharma <71224541+ArihantSharma@users.noreply.github.com>
2023-03-14 13:01:57 +0530

    Update start_text.py

--- main/start_text.py
+++ main/start_text.py
@@ -1,19 +1,6 @@
 from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup 
 from config import ADMIN
- 
-
-@Client.on_message(filters.command("start") & filters.private)                             
-async def start_cmd(bot, msg):
-    txt="This is personal use bot 🙏. Do you want your own bot? 👇 Click the source code to deploy"
-    btn = InlineKeyboardMarkup([[
-        InlineKeyboardButton("🤖 SOURCE CODE", url="https://github.com/MrMKN/Simple-Rename-Bot")
-        ],[
-        InlineKeyboardButton("🖥️ How To Deploy", url="https://youtu.be/oc847WvOUaI")
-    ]])
-    if msg.from_user.id != ADMIN:
-        return await msg.reply_text(text=txt, reply_markup=btn, disable_web_page_preview = True)
-    await start(bot, msg, cb=False)
 
 
 @Client.on_callback_query(filters.regex("start"))
